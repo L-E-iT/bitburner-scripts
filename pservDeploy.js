4042c325beab2fcb41e4720931b962b2c55ce976
Brian Elliott <ellio2bj@dukes.jmu.edu>
2022-10-09 14:41:29 -0400

    changing deploy

--- pservDeploy.js
+++ pservDeploy.js
@@ -1,12 +1,12 @@
 /** @param {NS} ns */
 export async function main(ns) {
 	let servers = ns.scan("home")
-	servers = servers.filter((server) => server.includes("pserv"))
 	servers.forEach((server) => {
-		let reqMem = ns.getScriptRam("early-hack-template.script");
+		// Assumption is that your script is in the home directory
+		let reqMem = ns.getScriptRam("hack.script");
 		let availMem = ns.getServerMaxRam(server);
 		let threads = Math.floor(availMem / reqMem);
-		ns.scp("early-hack-template.script", server);
-		ns.exec("early-hack-template.script", server, threads);
+		ns.scp("hack.script", server);
+		ns.exec("hack.script", server, threads);
 	});
 }
