1828a419a7ba23cf8cbe9ddcfcc1afddb4a55e2c
Brian Elliott <ellio2bj@dukes.jmu.edu>
2022-10-09 17:17:09 -0400

    updating deploy script

--- deploy-to-servers.js
+++ deploy-to-servers.js
@@ -3,8 +3,10 @@ import { main as deepscan } from "lib/deepscan.js"
 
 export async function main(ns) {
 	let servers = await deepscan(ns)
+
 	servers.forEach((server) => {
 		// Assumption is that your script is in the home directory
+		ns.killall(server)
 		let reqMem = ns.getScriptRam("hack.script");
 		let availMem = ns.getServerMaxRam(server);
 		let threads = Math.floor(availMem / reqMem);
